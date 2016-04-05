4c60884084e513390341ad6abf21334967e19ec3
devin <dmanhilpert@yahoo.com>
2016-04-05 00:22:37 -0400

    finished alpha

--- Wrath of Dabba2D/Assets/Scripts to Modify/UIDisplay.cs
+++ Wrath of Dabba2D/Assets/Scripts to Modify/UIDisplay.cs
@@ -27,7 +27,7 @@ public class UIDisplay : MonoBehaviour {
 
         Shooter_Script = GameObject.Find("Dabba's Shooter").GetComponent<DabbaShooter>(); //Get the Shooter Script from Dabba to access the power and number of uses
 
-        Show_Ability.text = "Ability: None"; //No ability initially
+        Show_Ability.text = "Ability"; //No ability initially
     }
 
     // Update is called once per frame
@@ -35,9 +35,9 @@ public class UIDisplay : MonoBehaviour {
     {
         if (Shooter_Script.Power != null) //If Dabba has acquired an ability (so Power is not null)
         {
-            Show_Ability.text = "Ability: " + Shooter_Script.Power.name; //Ability displayed is the name of the power on the Shooter Script
+            Show_Ability.text = Shooter_Script.Power.name; //Ability displayed is the name of the power on the Shooter Script
         }
-        Show_Uses.text = "Uses: " + Shooter_Script.uses.ToString(); //Convert the number of uses to a String for display
+        Show_Uses.text = "x" + Shooter_Script.uses.ToString(); //Convert the number of uses to a String for display
     }
 
     public void Took_Damage()
