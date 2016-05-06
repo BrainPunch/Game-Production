77892406a7e8d2ea6b1fed9103bcc29b1bfd7229
Kyle Ring <ringk2@rpi.edu>
2016-05-06 15:27:13 -0400

    Added Scattershot Tank and Hurting Victim. They'll need their palette swaps

---
+++ Wrath of Dabba2D/Assets/Scripts to Modify/ScatterProjectile.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScatterProjectile : MonoBehaviour {
+
+    Transform t;
+    public Vector3 Shot_start; //Make Vector3's public so they can be adjusted on spawn by the shooter
+
+    public float speed = 10f;
+    public UIDisplay Score_Tracker;
+
+    public Transform Dabba_Loc;
+
+    public Vector3 Dabba_start;
+
+    public float point_value = 0f; //Shot is worth no points, only meant to hurt Dabba
+
+    public float damage = 5f; //Value of damage
+
+    public Vector3 direction;
+
+    //Vector3 ScatterOffset; //Vector to change direction of a particular scattershot when instantiated.
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+
+    void Awake() { //For Scattershot, initialize these values on Awake() instead of on Start(). This ensures they have these values as soon as they exist
+
+        t = GetComponent<Transform>(); //Get Transform of this shot GameObject
+        Shot_start = transform.position;
+
+        Score_Tracker = GameObject.Find("Canvas").GetComponent<UIDisplay>();
+
+        Dabba_Loc = GameObject.Find("Dabba").GetComponent<Transform>(); //Get the Transform of Dabba at the time the shot is spawned
+        Dabba_start = Dabba_Loc.position; //Get Dabba's position when the projectile is spawned
+
+
+        direction = (Dabba_start - Shot_start); //Normalize the direction towards the point in question for use in movement
+        direction.Normalize();
+
+        t.position = new Vector3(t.position.x, t.position.y, t.position.y); //Set the position of the object so its z value matches its Y value for layering purposes        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Dabba_Loc == null)
+        {
+            Debug.Log("No Dabba Loc");
+        }
+
+
+        float step = speed * Time.deltaTime;
+        t.Translate(step * direction);
+
+        if (t.position.x > 20f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeDir(Vector3 offset) {
+                
+        direction = ( (Dabba_start + offset) - Shot_start).normalized; //Add the point of offset to the starting location and shot_start variables and renormalize
+        
+    }
+}
---
+++ Wrath of Dabba2D/Assets/Scripts to Modify/ScatterShooter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScatterShooter : MonoBehaviour {
+
+    public float StartOffset = 0f;
+
+    public GameObject ScatteredShot;
+
+    GameObject Shot1;
+    GameObject Shot2;
+
+    // Use this for initialization
+    void Start () {
+
+        Invoke("ScatterShoot", StartOffset); //Waits through StartOffset and invokes Shoot once
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void ScatterShoot()
+    {
+        float randomTime = Random.Range(3, 6); //Determine an amount of time within the range (exclusive) to invoke the function again
+
+        Shot1 = (GameObject)Instantiate(ScatteredShot, transform.position, Quaternion.identity); //Spawn 3 shots, need to make them scatter
+        
+        Shot2 = (GameObject)Instantiate(ScatteredShot, transform.position, Quaternion.identity); //Capture reference to the instantiated shot so their directions can be adjusted
+        
+        Instantiate(ScatteredShot, transform.position, Quaternion.identity); //Shot 3 should fly as normal
+
+        //Anything done to these instantiated shots in this function occurs before their own Start()
+        
+        ScatterProjectile Shot1Script = Shot1.GetComponent<ScatterProjectile>(); //Get the Scattershot's script
+        Shot1Script.ChangeDir(new Vector3(0f, 2f, 0f) ); //Change the direction by this particular offset using a function on the script
+
+        ScatterProjectile Shot2Script = Shot2.GetComponent<ScatterProjectile>();
+        Shot2Script.ChangeDir(new Vector3(0f, -2f, 0f)); //Change the direction by this particular offset
+
+        //Can change the Spread's width by adjusting the y value in the above Vector3's
+
+        Invoke("ScatterShoot", randomTime); //Invoke the shoot function again 
+    }
+}
